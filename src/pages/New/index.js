bfb5655e32e219ded63c05d45e9bc8da51bbb34b
kleyson castro <kleysoncastro99@gamil.com>
2019-10-02 20:57:22 -0300

    cadastro de spot feito

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,14 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState, useMemo} from 'react'
+import api from '../../servces'
 import camera from '../../assets/camera.svg'
 import './style.css'
 
-export default function New() {
+export default function New( {history}) {
 
+    const [thumbnail, setThumbnail] = useState(null);
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
     const [price, setPrice] = useState('');
+
+    const preview = useMemo(() => {
+
+        return thumbnail ? URL.createObjectURL(thumbnail) : null;
+
+    }, [thumbnail])// fim preview
+
    
-   function handleSubmit() {
+  async function handleSubmit(event) {
+
+    event.preventDefault();
+
+    const data = new FormData();
+    const user_id = localStorage.getItem('user')
+
+    data.append('thumbnail', thumbnail);
+    data.append('company', company);
+    data.append('techs', techs);
+    data.append('price', price);
+
+        await api.post('/spots', data, {
+        headers: {user_id}
+    })
+    history.push('/dashboard')
 
    }
    
@@ -16,8 +40,12 @@ export default function New() {
 
         <form onSubmit={handleSubmit}>
 
-        <label id="thumbnail">
-            <input type="file" />
+        <label 
+            id="thumbnail" 
+            style={{backgroundImage: `url(${preview})`}} 
+            className={thumbnail ? 'has-thumbnail' : ''}
+            >
+            <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
             <img src={camera} alt="slect image"/>
 
         </label>
