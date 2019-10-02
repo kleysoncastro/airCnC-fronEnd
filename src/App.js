cdf59071306b0e0b53798455c0e3cd0ab49c93e0
kleyson castro <kleysoncastro99@gamil.com>
2019-10-02 14:58:07 -0300

    parce de login concluida

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, {useState} from 'react';
-import api from './servces'
+import React from 'react';
 import './App.css';
 import logo from './assets/logo.svg'
 import Routes from './routes'
 
 function App() {
-
-  const [email, setEmail] = useState('');
-
- async function handleSubmit(event) {
-
-    // anula evendo dadrao do usuario
-    event.preventDefault();
- 
-    const response = await api.post('/sessions', {email})
-
-    const {_id} = response.data;
-
-    // salva no browser
-    localStorage.setItem('user', _id);
- 
-  }// fim handleSubmit
 
 
   return (
@@ -30,21 +13,6 @@ function App() {
      <img src={logo} alt="aircnc"/>
      <div className="content">
        <Routes />
-       <p>
-         Ofereça <strong>Sptos</strong> para programadores e contrate <strong>talentos</strong> para sua empresa
-       </p>
-    <form onSubmit={handleSubmit}>
-    <label htmlFor="email">Email *</label>
-    <input 
-          type="email" 
-          id="email" 
-          placeholder="Seu melhor e-mail"
-          value={email}
-          onChange={event => setEmail(event.target.value)}
-          />
-          <button className="btn" type="submit">Entrar</button>
-    </form>
-
      </div>
 
    </div>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+import api from '../../servces'
 
-export default function Login() {
-    return <div />
+
+/* 
+    * as funcoes da DOM sao recuperadas como parametro das funcoes
+    * history é um  parametro usado para navegacao nas paginas
+*/
+
+export default function Login({history}) {
+
+    const [email, setEmail] = useState('');
+
+    async function handleSubmit(event) {
+   
+       // anula evendo dadrao do usuario
+       event.preventDefault();
+    
+       const response = await api.post('/sessions', {email})
+   
+       const {_id} = response.data;
+   
+       // salva no browser
+       localStorage.setItem('user', _id);
+
+       history.push('/dashboard')
+    
+     }// fim handleSubmit
+
+
+    return (
+    <>
+        <p>
+         Ofereça <strong>Sptos</strong> para programadores e contrate <strong>talentos</strong> para sua empresa
+       </p>
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email *</label>
+            <input 
+          type="email" 
+          id="email" 
+          placeholder="Seu melhor e-mail"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+          />
+          <button className="btn" type="submit">Entrar</button>
+    </form>
+        </>
+    )
 
 }
