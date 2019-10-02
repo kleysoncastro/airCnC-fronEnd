a10ed1ae1f5a71951f7fb71df6a0a01abc2eb481
kleyson castro <kleysoncastro99@gamil.com>
2019-10-02 20:16:31 -0300

    selecao de img contruida e funcionando

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
+import camera from '../../assets/camera.svg'
+import './style.css'
 
 export default function New() {
-    return <div />
+
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+   
+   function handleSubmit() {
+
+   }
+   
+    return (
+
+        <form onSubmit={handleSubmit}>
+
+        <label id="thumbnail">
+            <input type="file" />
+            <img src={camera} alt="slect image"/>
+
+        </label>
+
+
+
+            <label htmlFor="company">Empresa *</label>
+            <input 
+            
+                id="company"
+                placeholder="Sua empresa incrível"
+                value={company}
+                onChange={event => setCompany(event.target.value)} 
+            />
+
+            <label htmlFor="techs">Tecnologias *<span>(separadas por vigula)</span></label>
+            <input 
+            
+                id="techs"
+                placeholder="Quais tecnologias usam?"
+                value={techs}
+                onChange={event => setTechs(event.target.value)} 
+            />
+
+            <label htmlFor="techs">Valor da diaria *<span>(Em branco para gratuito)</span></label>
+            <input 
+            
+                id="techs"
+                placeholder="Valor cobrado por dia"
+                value={price}
+                onChange={event => setPrice(event.target.value)} 
+            />
+
+        <button type="submit" className="btn">cadastrar</button>
+
+        </form>
+
+
+    )// fim return 
 }
