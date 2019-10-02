d9ef1c9a3a542c00cfcdd4409f2f1943848f37c3
kleyson castro <kleysoncastro99@gamil.com>
2019-10-02 15:36:53 -0300

    Listagem de spots cadastrados feito

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import api from '../../servces'
+
+/* 
+ * useEffect é usaca como uma lista observavel 
+ * sempre uma varias passadacnos [] sofre auteracao 
+ * a funcao e execurtada
+ * quando [] esta vazio so exec uma vez
+
+*/
+
 
 export default function Bashboard() {
-    return <div />
     
+    useEffect(()=>{
+
+        async function loadSposts() {
+            const user_id = localStorage.getItem('user')
+            const response = await api.get('/dashboard', {
+
+                headers: {user_id}
+            
+            });
+            console.log(response.data)
+        }
+
+        loadSposts();
+    }, [])
+    
+    return <h1>Dashboard</h1>
 }
